07427a6457fab32a5f33d00da29cca29cac328dd
hasbiardianto <rdnthasbi@gmail.com>
2024-08-13 16:22:03 +0700

    update type in api/todo/route.ts

--- app/api/items/route.ts
+++ app/api/items/route.ts
@@ -2,6 +2,7 @@ import { Item } from "@/app/type";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL as string;
 
+// Fetch items for a specific todo
 export async function fetchItems(todo_id: number, token: string | null): Promise<Item[]> {
     const response = await fetch(`${API_URL}/todos/${todo_id}/items`, {
         headers: {
@@ -13,6 +14,8 @@ export async function fetchItems(todo_id: number, token: string | null): Promise
     }
     return await response.json();
 }
+
+// Store a new item in a specific todo
 export async function storeItems(name: string, percentage: number, todo_id: number, token: string | null): Promise<Item> {
     const response = await fetch(`${API_URL}/todos/${todo_id}/items`, {
         method: "POST",
@@ -30,35 +33,36 @@ export async function storeItems(name: string, percentage: number, todo_id: numb
     }
     return await response.json();
 }
-export async function updateItems(token: string | null): Promise<Item[]> {
-    const response = await fetch(`${API_URL}items`, {
+
+// Update a specific item
+export async function updateItems(id: number, name: string, percentage: number, token: string | null): Promise<Item> {
+    const response = await fetch(`${API_URL}/items/${id}`, {
         method: "PUT",
         headers: {
             'Content-Type': 'application/json',
             'Authorization': `Bearer ${token}`,
         },
         body: JSON.stringify({
-            id: 1, // Replace with the id of the item you want to delete
+            name: name,
+            progress_percentage: percentage,
         }),
     });
     if (!response.ok) {
-        throw new Error("Failed to delete item");
+        throw new Error("Failed to update item");
     }
     return await response.json();
 }
-export async function destroyItems(token: string | null): Promise<Item[]> {
-    const response = await fetch(`${API_URL}items`, {
+
+// Delete a specific item
+export async function destroyItems(id: number, token: string | null): Promise<void> {
+    const response = await fetch(`${API_URL}/items/${id}`, {
         method: "DELETE",
         headers: {
             'Content-Type': 'application/json',
             'Authorization': `Bearer ${token}`,
         },
-        body: JSON.stringify({
-            id: 1, // Replace with the id of the item you want to delete
-        }),
     });
     if (!response.ok) {
         throw new Error("Failed to delete item");
     }
-    return await response.json();
 }
